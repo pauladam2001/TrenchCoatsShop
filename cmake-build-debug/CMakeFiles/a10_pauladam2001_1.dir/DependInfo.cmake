c9d2da47d324330d26d697d3a99701f517b7148e
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2021-10-03 20:57:23 +0300

    Commit

---
+++ cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/QtViewModel/tableModel.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/QtViewModel/tableModel.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/a10_pauladam2001_1_autogen/mocs_compilation.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/a10_pauladam2001_1_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/controller/service.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/controller/service.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/controller/userService.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/controller/userService.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/entity/trenchCoat.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/entity/trenchCoat.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/exceptions/customExceptions.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/exceptions/customExceptions.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/pictureDelegate/pictureDelegate.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/pictureDelegate/pictureDelegate.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/repository/CSVBasket.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/repository/CSVBasket.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/repository/HTMLBasket.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/repository/HTMLBasket.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/repository/fileRepository.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/repository/fileRepository.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/repository/repository.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/repository/repository.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/start.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/start.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/test/tests.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/test/tests.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/ui/GUI.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/ui/GUI.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/ui/userInterface.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/ui/userInterface.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/undoRedo/action.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/undoRedo/action.cpp.obj"
+  "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/validation/validators.cpp" "C:/Users/paula/OneDrive/Documents/GitHub/a10-pauladam2001-1/cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/validation/validators.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "a10_pauladam2001_1_autogen/include"
+  "../controller"
+  "../dynamicArray"
+  "../entity"
+  "../exceptions"
+  "../repository"
+  "../test"
+  "../ui"
+  "../validation"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/a10_pauladam2001_1.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a10_pauladam2001_1_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/a10_pauladam2001_1_autogen.dir/ParseCache.txt"
+  "a10_pauladam2001_1_autogen"
+  "CMakeFiles/a10_pauladam2001_1.dir/QtViewModel/tableModel.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/a10_pauladam2001_1_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/controller/service.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/controller/userService.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/entity/trenchCoat.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/exceptions/customExceptions.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/pictureDelegate/pictureDelegate.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/repository/CSVBasket.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/repository/HTMLBasket.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/repository/fileRepository.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/repository/repository.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/start.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/test/tests.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/ui/GUI.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/ui/userInterface.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/undoRedo/action.cpp.obj"
+  "CMakeFiles/a10_pauladam2001_1.dir/validation/validators.cpp.obj"
+  "a10_pauladam2001_1.exe"
+  "a10_pauladam2001_1.exe.manifest"
+  "a10_pauladam2001_1.pdb"
+  "liba10_pauladam2001_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a10_pauladam2001_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
